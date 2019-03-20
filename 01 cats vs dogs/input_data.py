549d41bc2c46ff26f2c5ce02c39f3e7fef46307e
HumanEric <171240139@qq.com>
2019-03-20 14:50:58 +0800

    first commit

--- 01 cats vs dogs/input_data.py
+++ 01 cats vs dogs/input_data.py
@@ -1,46 +1,8 @@
-#By @Kevin Xu
-#kevin28520@gmail.com
-
-# 11.08 2017 更新
-# 最近入驻了网易云课堂讲师，我的第一门课《使用亚马逊云计算训练深度学习模型》。
-# 有兴趣的同学可以学习交流。
-# * 我的网易云课堂主页： http://study.163.com/provider/400000000275062/index.htm
-
-# 深度学习QQ群, 1群满): 153032765
-# 2群：462661267
-#The aim of this project is to use TensorFlow to process our own data.
-#    - input_data.py:  read in data and generate batches
-#    - model: build the model architecture
-#    - training: train
-
-# I used Ubuntu with Python 3.5, TensorFlow 1.0*, other OS should also be good.
-# With current settings, 10000 traing steps needed 50 minutes on my laptop.
-
-
-# data: cats vs. dogs from Kaggle
-# Download link: https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/data
-# data size: ~540M
-
-# How to run?
-# 1. run the training.py once
-# 2. call the run_training() in the console to train the model.
-
-# Note: 
-# it is suggested to restart your kenel to train the model multiple times 
-#(in order to clear all the variables in the memory)
-# Otherwise errors may occur: conv1/weights/biases already exist......
-
-
-#%%
-
 import tensorflow as tf
 import numpy as np
 import os
 
-#%%
-
-# you need to change this to your data directory
-train_dir = '/home/kevin/tensorflow/cats_vs_dogs/data/train/'
+train_dir = '/home/dataset/kaggle/dog_cat/train/'
 
 def get_files(file_dir):
     '''
